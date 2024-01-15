f7eb904872647b76f1ef53cc856ae3a7dd9465a7
L. Maksym <mlobacov@gmail.com>
2024-01-15 10:42:58 +0200

    test

--- src/Components/Author/index.jsx
+++ src/Components/Author/index.jsx
@@ -11,10 +11,22 @@ const Author = () => {
           <div className='title-text'>
             <h3>Ольга Патієвич</h3>
             <ul className='title-list'>
-              <li className='title-list-item'>17 років у викладанні англійської студентам різних мовних рівнів та спеціальностей у Львівському університеті, ІТ компаніях та фінансових установах</li>
-              <li className='title-list-item'>Досвід тривалого проживання у Лондоні</li>
-              <li className='title-list-item'>Безумовна любов до студентів і викладання</li>
-              <li className='title-list-item'>Численні авторські розробки англомовних навчальних матеріалів авторка:</li>
+              <li className='title-list-item'>
+                <div className="title-svg"></div>
+                <span>17 років у викладанні англійської студентам різних мовних рівнів та спеціальностей у Львівському університеті, ІТ компаніях та фінансових установах</span>
+              </li>
+              <li className='title-list-item'>
+                <div className="title-svg"></div>
+                <span>Досвід тривалого проживання у Лондоні</span>
+              </li>
+              <li className='title-list-item'>
+                <div className="title-svg"></div>
+                <span>Безумовна любов до студентів і викладання</span>
+              </li>
+              <li className='title-list-item'>
+                <div className="title-svg"></div>
+                <span>Численні авторські розробки англомовних навчальних матеріалів авторка:</span>
+              </li>
                 <ul className='title-sublist'>
                   <li className='title-sublist-item'>- Курсу «English Tenses: Weekend with Eddie»</li>
                   <li className='title-sublist-item'>- Курсу «General English»</li>
@@ -22,8 +34,14 @@ const Author = () => {
                   <li className='title-sublist-item'>- «English planner»</li>
                   <li className='title-sublist-item'>- Статей у численних наукових виданнях України та світу</li>
                 </ul>
-              <li className='title-list-item'>Багаторічний досвід проведення англомовних квестів, ігор, олімпіад та створення е-курсів</li>
-              <li className='title-list-item'>Додаткові бонуси для потіхи власного его та рівня кваліфікації: диплом кандидата педагогічних наук та атестат доцента</li>
+              <li className='title-list-item'>
+                <div className="title-svg"></div>
+                <span>Багаторічний досвід проведення англомовних квестів, ігор, олімпіад та створення е-курсів</span>
+              </li>
+              <li className='title-list-item'>
+                <div className="title-svg"></div>
+                <span>Додаткові бонуси для потіхи власного его та рівня кваліфікації: диплом кандидата педагогічних наук та атестат доцента</span>
+              </li>
             </ul>
           </div>
         </div>
