6e984c0d63381ea5a09b9f32d777e9b85bc24f04
L. Maksym <mlobacov@gmail.com>
2024-01-18 16:06:26 +0200

    test

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -11,23 +11,23 @@ const Footer = () => {
           </div>
           <div className='menu-box'>
             <h2>Меню</h2>
-            <div className='menu-subbox'>
-              <p>Для кого курс?</p>
-              <p>Про курс</p>
-              <p>Опис курсу</p>
-              <p>Що ти отримаєш?</p>
-              <p>Про автора</p>
-            </div>
+            <ul className='menu-subbox'>
+              <li><a href="#target">Для кого курс?</a></li>
+              <li><a href="#about">Про курс</a></li>
+              <li><a href="#description">Опис курсу</a></li>
+              <li><a href="#result">Що ти отримаєш?</a></li>
+              <li><a href="#author">Про автора</a></li>
+            </ul>
           </div>
           <div className='resource-box'>
             <h2>Ресурси</h2>
-            <div className='resource-subbox'>
-              <p>Help</p>
-              <p>Cookie Settings</p>
-              <p>IP Policy</p>
-              <p>Privacy Policy</p>
-              <p>Term of Service</p>
-            </div>
+            <ul className='resource-subbox'>
+              <li>Help</li>
+              <li>Cookie Settings</li>
+              <li>IP Policy</li>
+              <li>Privacy Policy</li>
+              <li>Term of Service</li>
+            </ul>
           </div>
           <div className='register-box'>
             <h2>Зареєструйся на курс</h2>
